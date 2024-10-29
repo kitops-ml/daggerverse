879a69f3f56c4314d18f54074f60bdaaf1db29b5
Gorkem Ercan <gorkem.ercan@gmail.com>
2024-10-29 11:15:48 -0400

    updates for hf module
    Updates the dagger engine and dependencies.
    Improves error handling.

--- huggingface/dagger/main.go
+++ huggingface/dagger/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"dagger/huggingface/internal/dagger"
+	"fmt"
 )
 
 const (
@@ -18,8 +19,8 @@ func (m *Huggingface) baseContainer() *dagger.Container {
 		WithoutEntrypoint().
 		//Set it to $HOME
 		WithWorkdir("/home/nonroot").
-		WithExec([]string{"pip", "install", "-U", "huggingface_hub[cli]"} ).
-		WithExec([]string{"pip", "install", "-U", "huggingface_hub[hf_transfer]"} ).
+		WithExec([]string{"pip", "install", "-U", "huggingface_hub[cli]"}).
+		WithExec([]string{"pip", "install", "-U", "huggingface_hub[hf_transfer]"}).
 		WithEnvVariable("HF_HUB_ENABLE_HF_TRANSFER", "1")
 }
 
@@ -28,11 +29,16 @@ func (m *Huggingface) DownloadRepo(ctx context.Context,
 	// the Huggingface repository to download.
 	hfrepo string,
 	// the Huggingface secret token for authentication
-	secret *dagger.Secret) *dagger.Directory {
-	return m.baseContainer().
+	secret *dagger.Secret) (*dagger.Directory, error) {
+	c := m.baseContainer().
 		WithSecretVariable("HF_TOKEN", secret).
-		WithExec([]string{hfCliPath, "download", hfrepo, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"}).
-		Directory(localRepoDir)
+		WithExec([]string{hfCliPath, "download", hfrepo, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"})
+
+	_, err := c.Stdout(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download repo %s: %w", hfrepo, err)
+	}
+	return c.Directory(localRepoDir), nil
 }
 
 // Downloads a single file from Huggingface repo and returns the File
@@ -40,12 +46,16 @@ func (m *Huggingface) DownloadFile(ctx context.Context,
 	// the Huggingface repository to download.
 	hfrepo string,
 	// The path of the file to download
-	path string, 
+	path string,
 	// the Huggingface secret token for authentication
-	secret *dagger.Secret) *dagger.File {
-	return m.baseContainer().
+	secret *dagger.Secret) (*dagger.File, error) {
+	c := m.baseContainer().
 		WithSecretVariable("HF_TOKEN", secret).
-		WithExec([]string{hfCliPath, "download", hfrepo, path, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"}).
-		Directory(localRepoDir).
-		File(path)
+		WithExec([]string{hfCliPath, "download", hfrepo, path, "--local-dir", localRepoDir, "--token", "$HF_TOKEN"})
+	_, err := c.Stdout(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download %s from %s: %w", path, hfrepo, err)
+	}
+
+	return c.Directory(localRepoDir).File(path), nil
 }
