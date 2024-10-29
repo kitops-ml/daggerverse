436badfe316a9c8d3eec180f1c0cd5e0ff2b68b9
Gorkem Ercan <gorkem.ercan@gmail.com>
2024-10-29 16:22:29 -0400

    Update gguf module
    Updates the gguf module for the latest dagger engine.
    Improves error handling. Moves baseContainer to be
    private.

--- gguf/dagger/main.go
+++ gguf/dagger/main.go
@@ -14,7 +14,7 @@ const (
 
 type Gguf struct{}
 
-func (m *Gguf) BaseContainer() *dagger.Container {
+func (m *Gguf) baseContainer() *dagger.Container {
 	return dag.Container().
 		From(llamacppImageRef).
 		WithoutEntrypoint()
@@ -24,19 +24,24 @@ func (m *Gguf) BaseContainer() *dagger.Container {
 // outfile: the output file name for the converted model.
 // Returns the resulting file
 func (m *Gguf) ConvertToGGuf(
-
+	ctx context.Context,
 	// the directory containing the source model.
 	source *dagger.Directory,
 	// additional parameters to pass to the conversion script.
 	// +optional
-	parameters ...string) *dagger.File {
+	parameters ...string) (*dagger.File, error) {
 
 	execWord := []string{"python3", ggufConvertScript, "/src", "--outfile", convertedFileName}
 	execWord = append(execWord, parameters...)
-	return m.BaseContainer().
+	container := m.baseContainer().
 		WithMountedDirectory("/src", source).
-		WithExec(execWord).
-		File(convertedFileName)
+		WithExec(execWord)
+
+	_, err := container.Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return container.File(convertedFileName), nil
 }
 
 // Quantize applies quantization to a given model file.
@@ -44,16 +49,23 @@ func (m *Gguf) Quantize(ctx context.Context,
 	// the source model file to be quantized.
 	source *dagger.File,
 	// the quantization parameter to apply.
-	quantization string) *dagger.File {
+	quantization string) (*dagger.File, error) {
 
 	modelname, err := source.Name(ctx)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	execWord := []string{"/app/llama-quantize", modelname, quantizedFileName, quantization}
-	return m.BaseContainer().
+	container := m.baseContainer().
 		WithMountedFile(modelname, source).
-		WithExec(execWord).
-		File(quantizedFileName)
+		WithExec(execWord)
+
+	_, err = container.Stdout(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return container.File(quantizedFileName), nil
 }
